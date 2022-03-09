2fef0daac912218281cf168b16709833c9297104
jaejun-lee06 <99355321+jaejun-lee06@users.noreply.github.com>
2022-03-09 18:09:16 +0000

    daily assignment

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Copyright (c) 2020 Mr. Coxall All rights reserved
+//
+// Created by: Jaejun Lee
+// Created on: Mar 2022
+// This file contains the JS functions for index.html
+
+function myButtonClicked() {
+  // this function displays "Hello, World!"
+
+  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>";
+}
